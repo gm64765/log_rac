c774c7485162be93e174b750b7b55cecca561cc6
gm64765 <gm64765@student.uni-lj.si>
2024-05-03 13:02:02 +0200

    data added

--- sat solver/sat_solver.py
+++ sat solver/sat_solver.py
@@ -1,3 +1,21 @@
 #
 # Here we will write code for sat solver
 #
+
+
+"""
+Input is in CNF DIMACS FORMAT:
+
+p cnf <num_vars> <num_clauses>
+<clause1>
+<clause2>
+...
+<clauseN>
+
+Each clause contains disjunctions.
+All clauses are connected by conjunctions.
+
+Example: (x1 v x2) & (x2 v x3) & (x2 v x4)
+
+"""
+
