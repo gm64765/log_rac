5ca50e5a1fbb1c753ecc7fe717a06a5fbcbe4445
gm64765 <gm64765@student.uni-lj.si>
2024-05-27 13:12:14 +0200

    still in progress

--- sat solver/sat_solver.py
+++ sat solver/sat_solver.py
@@ -27,27 +27,42 @@ def unit_propagate(literal, clauses):
     for c in clauses:
         
         if literal * (-1) in c:
+            #
+            # the negation of a literal occurs in a clause - we simplify the clause by removing the literal negation 
+            #
             c_new = copy.deepcopy(c)
             c_new.remove(literal * (-1))
             simplified_clauses.append(c_new)
 
         elif literal not in c:
+            #
+            # the literal nor its negation occur in a clause - we keep the whole clause
+            #
             c_new = copy.deepcopy(c)
             simplified_clauses.append(c_new)
 
     return simplified_clauses
 
 def exists_unit_clause(clauses):
+    #
+    # Return True if there exists a unit clause, otherwise False
+    #
     for el in clauses:
         if len(el) == 1:
             return True
     return False
 
 def return_unit_clause_literal(clauses):
+    #
+    # returns a unit class literall if it exists, otherwise returns 0
+    #
     for el in clauses:
         if len(el) == 1:
             return el[0]
     return 0
+
+global DPLL_final_result 
+DPLL_final_result = []
 
 def DPLL (clauses, true_variables, literals_to_check):
     """
@@ -56,23 +71,34 @@ def DPLL (clauses, true_variables, literals_to_check):
     Returns:
     
     """
-    if len(clauses) == 0:
-        return True
-    elif [] in clauses:
-        return False
+    print("checking")
+    print(literals_to_check)
+    print(clauses)
+    print("true variables")
+    print(true_variables)
     
-    next_clauses = []
+    if len(literals_to_check) == 0:
+        if len(clauses) == 0:
+            global DPLL_final_result 
+            DPLL_final_result = true_variables
+            print("hura")
+            return True
+        elif [] in clauses:
+            return False
+    
+    next_clauses = clauses
     new_true_variables = []
     #
     # Unit propagation
 
-    while exists_unit_clause(clauses):
-        literal = return_unit_clause_literal(clauses)
-        clauses = unit_propagate(literal, clauses)
-        #print(literal)
+    while exists_unit_clause(next_clauses):
+        # return a unit clause literal
+        literal = return_unit_clause_literal(next_clauses)
+        # simplify clauses and keep only those that do not include literal or they contain literal negation  
+        next_clauses = unit_propagate(literal, next_clauses)
+        if literal not in new_true_variables:
+            new_true_variables.append(literal)
 
-    #print("clauses1")
-    #print(clauses)
     #
     # Pure literal elemination
     
@@ -80,22 +106,27 @@ def DPLL (clauses, true_variables, literals_to_check):
         #Heuristics    
     #
     
+    print("len", len(literals_to_check))
     l = literals_to_check[0] # pozitivna vrednost int
     literals_to_check.remove(l)
 
     #case 1 recimo, da l je resničen
     # če je l resničen, so zadovoljivi vsi clausi, ki vsebujejo l
     # clausi z -l so zadovoljivi, če je preostanek clausa zadovoljiv: False v C == True iff C == True 
-    next_clauses_case1 = copy.deepcopy(clauses)
+    next_clauses_case1 = copy.deepcopy(next_clauses)
+    true_variables_case1 = copy.deepcopy(true_variables)
+    true_variables_case1 = true_variables_case1 + new_true_variables
+    true_variables_case1.append(l)
     
     #case 2 recimo, da l ni resničen
     # če je -l resničen, so zadovoljivi vsi clausi, ki vsebujejo -l
     # clausi z l so zadovoljivi, če je preostanek clausa zadovoljiv: False v C == True iff C == True 
-    next_clauses_case2 = copy.deepcopy(clauses)
+    next_clauses_case2 = copy.deepcopy(next_clauses)
+    true_variables_case2 = copy.deepcopy(true_variables)
+    true_variables_case2 = true_variables_case2 + new_true_variables
+    true_variables_case1.append((-1 * l))
+    
 
-    #print("next_clauses")
-
-    #print(next_clauses)
     for c in next_clauses:
         # c vsebuje l
         if l in c:
@@ -124,17 +155,13 @@ def DPLL (clauses, true_variables, literals_to_check):
 
         if l not in c or (-1) * l not in c:
             c_new = copy.deepcopy(c)
-            c_new.remove(l)
             next_clauses_case1.append(c_new)
             next_clauses_case2.append(c_new)
         # c vsebuje -l
 
         # c ne vsebuje l, dodamo c v oba lista
-    #print("hello")
-    #print (next_clauses_case1)
-    #print (next_clauses_case2)
 
-    return DPLL (next_clauses_case1, true_variables, literals_to_check) or DPLL (next_clauses_case2, true_variables, literals_to_check)
+    return DPLL (next_clauses_case1, true_variables_case1, literals_to_check) or DPLL (next_clauses_case2, true_variables_case2, literals_to_check)
 
 if __name__=="__main__":
     t_start = time.time()
@@ -142,16 +169,13 @@ if __name__=="__main__":
     # Start reading after reaching the first line of dimacs format (p cnf <num_vars> <num_clauses>).
     save_clauses = False
     
-    #
     num_vars = 0 
-    
-    #
     num_clauses = 0
 
     all_clauses = []
 
-    #with open('input_output/my_example.txt', 'r') as file:
-    with open('input_output/sudoku_easy.txt', 'r') as file:
+    with open('input_output/my_example.txt', 'r') as file:
+    #with open('input_output/sudoku_easy.txt', 'r') as file:
 
         for line in file:
             symbols = line.strip().split()
@@ -173,5 +197,8 @@ if __name__=="__main__":
     literals_to_check = [i for i in range(1, num_vars + 1)]
     sol = DPLL(all_clauses, [], literals_to_check)
     print(sol)
+    print("solution:")
+    print(DPLL_final_result)
+
     t_end = time.time() - t_start
     print(t_end)
